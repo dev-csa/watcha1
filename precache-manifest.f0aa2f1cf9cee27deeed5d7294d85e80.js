c1d654cd0e9291fc8edaf58242cc8926ef2723a1
CSA <csarising@gmail.com>
2020-03-02 05:11:44 +0900

    Updates

---
+++ precache-manifest.f0aa2f1cf9cee27deeed5d7294d85e80.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0df5202c67e095d66e1033bdc3798f98",
+    "url": "/watcha1/index.html"
+  },
+  {
+    "revision": "0b2365ff5d35e6a86891",
+    "url": "/watcha1/static/js/2.c96cb15d.chunk.js"
+  },
+  {
+    "revision": "aeb12352f7207db58f0f4bd0569df687",
+    "url": "/watcha1/static/js/2.c96cb15d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19a9e4e70de157c19376",
+    "url": "/watcha1/static/js/main.3a66ff01.chunk.js"
+  },
+  {
+    "revision": "d5095511b5e79b50f8ff",
+    "url": "/watcha1/static/js/runtime-main.4a9e0015.js"
+  }
+]);
